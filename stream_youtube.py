f0a921551cf607562a0f0f30b42127460ab7c299
stormxdev2 <168479104+stormxdev2@users.noreply.github.com>
2024-05-12 23:26:35 +0300

    Update stream_youtube.py

--- stream_youtube.py
+++ stream_youtube.py
@@ -1,41 +1,11 @@
-import streamlink
 import subprocess
 
-# Video ID of the YouTube video to stream
-video_id = "-p5NXiuZydw"
+def stream_youtube(video_id, stream_url, backup_url):
+    video_url = f'https://www.youtube.com/watch?v={video_id}'
+    subprocess.run(['youtube-dl', '-f', 'best', video_url])
 
-# RTMP URLs provided by YouTube
-stream_url = "rtmp://x.rtmp.youtube.com/live2"
-backup_url = "rtmp://y.rtmp.youtube.com/live2?backup=1"
-
-def get_best_stream_url(video_url):
-    # Fetch available streams for the given YouTube video URL
-    streams = streamlink.streams(video_url)
-    if 'best' in streams:
-        return streams['best'].url
-    return None
-
-def stream_youtube(video_id, primary_url, backup_url):
-    # Construct the YouTube video URL
-    video_url = f"https://www.youtube.com/watch?v={video_id}"
-
-    # Get the best available stream URL using streamlink
-    stream_url = get_best_stream_url(video_url)
-    if not stream_url:
-        print("Failed to get the stream URL.")
-        return
-
-    print(f"Streaming from: {stream_url}")
-
-    # Use ffmpeg to stream to the primary RTMP server URL with a backup option
-    ffmpeg_cmd = [
-        "ffmpeg", "-i", stream_url, "-c", "copy", "-f", "flv",
-        f"{primary_url}/{video_id}",
-        f"{backup_url}/{video_id}"
-    ]
-
-    # Start streaming using ffmpeg
-    subprocess.run(ffmpeg_cmd)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    video_id = '-p5NXiuZydw'
+    stream_url = 'rtmp://x.rtmp.youtube.com/live2'
+    backup_url = 'rtmp://y.rtmp.youtube.com/live2?backup=1'
     stream_youtube(video_id, stream_url, backup_url)
